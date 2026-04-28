bdd9dc18ceffc2a2cc31a46d2333cd8b60aae4e1
Abid <abidroid1@gmail.com>
2026-04-28 17:10:57 +0500

    Dashboard Screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_practice/screens/add_task_screen.dart';
 import 'package:firebase_practice/screens/login_screen.dart';
 import 'package:firebase_practice/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: (){
+
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+          return AddTaskScreen();
+
+        }));
+      }, child: Icon(Icons.add),),
       appBar: AppBar(
         backgroundColor: Colors.cyanAccent,
         title: Text("DASHBOARD"),
@@ -24,10 +32,29 @@ class _DashboardScreenState extends State<DashboardScreen> {
             }));
           }, icon:Icon(Icons.person) ),
           IconButton(onPressed: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (context){
-              return LoginScreen();
 
-            }));
+            showDialog(context: context, builder: (context){
+              return AlertDialog(
+                title: Text('Confirmation'),
+                content: Text('Are you sure to LogOut ?'),
+                actions: [
+                  TextButton(onPressed: (){
+                    Navigator.of(context).pop();
+                  }, child: Text('NO'))  ,
+                  TextButton(onPressed: (){
+                    Navigator.of(context).pop();
+
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                      return LoginScreen();
+
+                    }));
+                  }, child: Text('YES'))  ,
+                ],
+              );
+            });
+
+
+
           }, icon:Icon(Icons.logout) ),
 
         ],
